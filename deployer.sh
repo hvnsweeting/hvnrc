dfa1438e306063cfb7948dfe8384b619d780a13a
HVN <hvnsweeting@gmail.com>
2012-10-23 21:36:22 +0700

    update

--- deployer.sh
+++ deployer.sh
@@ -1,6 +1,7 @@
 #1/bin/bash
 
+echo "Running deployer, this file should run with '. deployer.sh'"
 cp vimrc_atvcc ~/.vimrc
 cp bashrc ~/.bashrc
 cp bash_aliases ~/.bash_aliases
-. ~/.bash_aliases
+source ~/.bash_aliases
