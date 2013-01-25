b15bd97c2e71fd105a87e014a98c376d2144d60f
HVN <hvn@familug.org>
2013-01-25 09:09:02 +0700

    deploy i3

--- deployer.sh
+++ deployer.sh
@@ -4,4 +4,5 @@ echo "Running deployer, this file should run with '. deployer.sh'"
 cp vimrc_atvcc ~/.vimrc
 cp bashrc ~/.bashrc
 cp hvnalias ~/.hvnalias
+cp i3config ~/.i3/config
 source ~/.hvnalias
