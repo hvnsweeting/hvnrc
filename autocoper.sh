ef35c9c08abeabeee002cf3df18079c0100f3042
hvnsweeting <hvnsweeting@gmail.com>
2013-06-14 18:17:24 +0000

    add tmux

--- autocoper.sh
+++ autocoper.sh
@@ -4,7 +4,13 @@ MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
 MY_PATH=$MY_PATH"/"
 
+set -x
 rsync -avz ~/.vimrc $MY_PATH"vimrc"
 rsync -avz ~/.i3/config $MY_PATH"i3config"
 rsync -avz ~/.hvnalias $MY_PATH"hvnalias"
 rsync -avz ~/.bashrc $MY_PATH"bashrc"
+rsync -avz ~/.Xresources $MY_PATH"Xresources"
+rsync -avz ~/.tmux.conf $MY_PATH"tmux.conf"
+rsync -avz ~/.xinitrc $MY_PATH"xinitrc"
+
+set +x
