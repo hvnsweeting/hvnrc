aa9a7907a9ffc0cbece0a9e9322c9e8603806f63
Viet Hung Nguyen <hvn@familug.org>
2021-01-18 12:14:39 +0700

    Remove old stuff

--- setupvim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.vim/{autoload, bundle}
-python2 -c "import urllib, os;\
-    print urllib.urlretrieve('https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim', \
-    os.path.join(os.path.expanduser('~'), '.vim', 'autoload', 'pathogen.vim'))"
